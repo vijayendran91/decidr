eaa81e2f02701ced22600015bbcb63c2d9b0890b
Vijay <vijayendran91@gmail.com>
2025-07-30 16:15:13 +1000

    chore: First Name Validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,15 @@ class Person < ApplicationRecord
 
   has_many :person_locations, dependent: :destroy
   has_many :locations, through: :person_locations
+
+  validates_presence_of :first_name
+
+  # validate :must_have_one_valid_affiliation
+
+
+  # def must_have_one_valid_affiliation
+  #   if affiliations.empty?
+  #     errors.add(:affiliations, "must include at least one")
+  #   end
+  # end
 end
